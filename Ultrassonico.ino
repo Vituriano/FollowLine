559771dd28211d64a9cea989af1c85834f4b5df3
vituriano <viturianooliveira@gmail.com>
2019-10-29 13:54:16 -0400

    First Commit

---
+++ Ultrassonico.ino
@@ -0,0 +1,43 @@
+/**********************************************************
+* UNIVERSIDADE FEDERAL DE PERNAMBUCO - UFPE
+* CENTRO DE INFORMARICA - CIN
+* INTRODUCAO A PROGRAMACAO COM GALILEO - IF668 EC
+* CODIGO PARA TESTE DO SHIELD SENSOR ULTRASSONICO
+********************************************************/
+
+#include "Ultrasonic.h"
+#define echoPin 10 //Pino 10 recebe o pulso do echo
+#define trigPin 8 //Pino 9 envia o pulso para gerar o echo
+
+//iniciando a função e passando os pinos
+Ultrasonic ultrasonic(trigPin,echoPin);
+
+int parou = 0;
+ 
+void setup()
+{
+   pinMode(echoPin, INPUT); // define o pino 10 como entrada (recebe)
+   pinMode(trigPin, OUTPUT); // define o pino 9 como saida (envia)
+   Serial.begin(9600);
+}
+ 
+void loop()
+{
+//seta o pino 9 com um pulso baixo "LOW" ou desligado ou ainda 0
+digitalWrite(trigPin, LOW);
+// delay de 2 microssegundos
+delayMicroseconds(2);
+//seta o pino 9 com pulso alto "HIGH" ou ligado ou ainda 1
+digitalWrite(trigPin, HIGH);
+//delay de 10 microssegundos
+delayMicroseconds(10);
+//seta o pino 9 com pulso baixo novamente
+digitalWrite(trigPin, LOW);
+// função Ranging, faz a conversão do tempo de
+//resposta do echo em centimetros, e armazena
+//na variavel distancia
+int distancia = (ultrasonic.Ranging(CM));
+Serial.print("Distancia em CM: ");
+Serial.println(distancia);
+delay(1000); //espera 1 segundo para fazer a leitura
+}
---
+++ ponte-h.ino
@@ -0,0 +1,67 @@
+/**********************************************************
+* UNIVERSIDADE FEDERAL DE PERNAMBUCO - UFPE
+* CENTRO DE INFORMARICA - CIN
+* INTRODUCAO A COMPUTAÇÃO COM GALILEO - IF668 EC
+* CODIGO PARA TESTE DO SHIELD DRIVER PONTE-H
+***********************************************************/
+ 
+//Definicoes pinos Galileo ligados a entrada da Ponte H
+int Motor_E1 = 6;
+int Motor_E2 = 7;
+int Motor_EV = 9;
+int Motor_DV = 3;
+int Motor_D2 = 4;
+int Motor_D1 = 5;
+  
+void setup()
+{
+  //Define os pinos como saida
+ pinMode(Motor_E1, OUTPUT);
+ pinMode(Motor_E2, OUTPUT);
+ pinMode(Motor_EV, OUTPUT);
+ pinMode(Motor_DV, OUTPUT);
+ pinMode(Motor_D2, OUTPUT);
+ pinMode(Motor_D1, OUTPUT);
+}
+  
+void loop()
+{
+ //Colocando o motor na velocidade máxima.
+ digitalWrite(Motor_EV, HIGH);
+ digitalWrite(Motor_DV, HIGH);
+ 
+ 
+ //Gira o Motor E no sentido horario
+ digitalWrite(Motor_E1, HIGH);
+ digitalWrite(Motor_E2, LOW);
+ delay(2000);
+ //Para o motor E
+ digitalWrite(Motor_E1, HIGH);
+ digitalWrite(Motor_E2, HIGH);
+ delay(500);
+ //Gira o Motor D no sentido horario
+ digitalWrite(Motor_D1, HIGH);
+ digitalWrite(Motor_D2, LOW);
+ delay(2000);
+ //Para o motor D
+ digitalWrite(Motor_D1, HIGH);
+ digitalWrite(Motor_D2, HIGH);
+ delay(500);
+ 
+ //Gira o Motor E no sentido anti-horario
+ digitalWrite(Motor_E1, LOW);
+ digitalWrite(Motor_E2, HIGH);
+ delay(2000);
+ //Para o motor E
+ digitalWrite(Motor_E1, HIGH);
+ digitalWrite(Motor_E2, HIGH);
+ delay(500);
+ //Gira o Motor D no sentido anti-horario
+ digitalWrite(Motor_D1, LOW);
+ digitalWrite(Motor_D2, HIGH);
+ delay(2000);
+ //Para o motor D
+ digitalWrite(Motor_D1, HIGH);
+ digitalWrite(Motor_D2, HIGH);
+ delay(500);
+}
