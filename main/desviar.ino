8cba03ee52033f91cccdd47a7583d87dd9102d62
vituriano <viturianooliveira@gmail.com>
2019-11-05 13:19:42 -0400

    Seguindo linha e desviando

--- main/desviar.ino
+++ main/desviar.ino
@@ -1,3 +1,17 @@
 void desviar(){
-  
+  parar();
+  delay(500);
+  viraDireita(100, 100);
+  delay(1000);
+  frente(100, 100);
+  delay(4000);
+  viraEsquerda(100, 100);
+  delay(1000);
+  frente(100, 100);
+  delay(4000);
+  viraEsquerda(100, 100);
+  delay(1000);
+  frente(100,100);
+  viraDireita(100,100);
+  delay(800);
 }
